aecc161fa71e20c1f7c55551eea2673fa5d4da59
Imac513 <103855112+Imac513@users.noreply.github.com>
2022-04-20 15:00:56 -0700

    Add files via upload

---
+++ AutonomousNavigation/MotorDriver/b_code_Skidsteering_teleop.ino
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Int16, Int16MultiArray, String
+from sensor_msgs.msg import Joy
+
+def startup():
+    rospy.init_node('teleop_node', anonymous=False)
+    rospy.Subscriber("joy", Joy, teleop, queue_size=1)
+    rospy.spin()
+    
+def teleop(data):
+    pub = rospy.Publisher('py_control', Int16MultiArray, queue_size = 1)
+
+
+        b = [0,0,0,0,0]
+        Msg = Int16MultiArray()
+        b = [data.axes[1],data.axes[0],data.axes[4],data.axes[3],1]
+        // b = [FB,LR,Speed 1 and 3,Speed 2 and 4,Current Speed]
+        if data.axes[4] > 0.9:
+        b[4] = 2
+        if data.axes[4] < -0.9:
+        b[4] = 4
+        if data.axes[3] < -0.9:
+        b[4] = 3
+        If data.axes[3] > 0.9:
+        b[4] = 1
+
+        msg.data = b
+        pub.publish(msg)
+
+if __name__ == '__main__':
+    try:
+        startup()
+    except rospy.ROSInterruptException:
+        pass
+
+
+
+
+

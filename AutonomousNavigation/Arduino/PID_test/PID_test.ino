4be922e26fc833067de263d8f3c1140ec17f117e
Luis Castro <90741466+LCcoding123@users.noreply.github.com>
2022-05-07 12:32:11 -0700

    Update PID_test.ino

--- AutonomousNavigation/Arduino/PID_test/PID_test.ino
+++ AutonomousNavigation/Arduino/PID_test/PID_test.ino
@@ -8,11 +8,13 @@ DualG2HighPowerMotorShield18v22 md;
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 Adafruit_BNO055 bno = Adafruit_BNO055();
 
+//PID setup
 double yaw_input = 0, yaw_output = 0, yaw_setpoint = 0;
-double yaw_kP = 5.5, yaw_kI = 1, yaw_kD = 0;
+double yaw_kP = 5.5, yaw_kI = 1, yaw_kD = 0; //values may vary for different results needed, trial and error
                   
 PID yaw_PID (&yaw_input, &yaw_output, &yaw_setpoint, 
               yaw_kP, yaw_kI, yaw_kD, DIRECT);
+
 
 void setup() {
   Serial.begin(115200);
@@ -27,6 +29,7 @@ void setup() {
   delay(1000);
 }
 
+//Initualizes BNO055; checks if connected properly
 void InitIMU() {
   Wire.beginTransmission(0x28);
   if (!bno.begin()) {
@@ -36,7 +39,7 @@ void InitIMU() {
   bno.setExtCrystalUse(true);
 }
 
-
+//Initializes PID library limits and mode
 void InitPID() {
   yaw_PID.SetMode(AUTOMATIC);
   yaw_PID.SetOutputLimits(-100, 100);
@@ -52,7 +55,6 @@ void loop() {
   Output(yaw_output);
   delay(100);
 }
-
 
 void IMU_Angle() {
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);

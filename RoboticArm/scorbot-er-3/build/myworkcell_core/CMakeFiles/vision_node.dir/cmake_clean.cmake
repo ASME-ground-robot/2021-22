c4a0b58274c3f5ee691fcc0e5318c08d32c73066
j-505 <54735464+j-505@users.noreply.github.com>
2022-05-12 17:00:05 -0700

    Add files via upload

---
+++ RoboticArm/scorbot-er-3/build/myworkcell_core/CMakeFiles/vision_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vision_node.dir/src/vision_node.cpp.o"
+  "/home/ros-industrial/scorbot1/devel/lib/myworkcell_core/vision_node.pdb"
+  "/home/ros-industrial/scorbot1/devel/lib/myworkcell_core/vision_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vision_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ RoboticArm/scorbot-er-3/build/myworkcell_core/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros-industrial/scorbot1/src/myworkcell_core
+# Build directory: /home/ros-industrial/scorbot1/build/myworkcell_core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
